4b1bd2f955311f8da813cc43434c8b7a7b002215
Nawal-Dweik <nawal_farouk@outlook.com>
2021-02-21 00:43:26 +0200

    make cards as one component with different props

--- fun_learning/src/views/Dashboard.js
+++ fun_learning/src/views/Dashboard.js
@@ -12,8 +12,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import DashboardMain from "../static/img/dashboard_main.jpg";
 import Variable from "../static/img/variable_icon.png";
+import If from "../static/img/if_icon.png";
+import Loop from "../static/img/loop_icon.png";
 import Header from "./Header";
 import Description from "./LevelsDescription";
+import Level from "./Level";
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -30,20 +33,7 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
   },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
 }));
-
-const cards = [1, 2, 3];
 
 export default function Album(props) {
   const classes = useStyles();
@@ -63,31 +53,9 @@ export default function Album(props) {
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image={Variable}
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe
-                      the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" className="play-button">
-                      Play
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
+            <Level image={Variable} title="Level1" description="description1" />
+            <Level image={If} title="Level2" description="description2" />
+            <Level image={Loop} title="Level3" description="description3" />
           </Grid>
         </Container>
       </main>
