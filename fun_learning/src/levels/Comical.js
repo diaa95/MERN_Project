f3271e6911cece6c75257738c6c043e25beaf64f
Diaa AbdAlDayem <Diaa.abdaldayem@gmail.com>
2021-02-24 22:42:01 +0200

    Animation Styled

--- fun_learning/src/levels/Comical.js
+++ fun_learning/src/levels/Comical.js
@@ -6,15 +6,20 @@ import React from "react";
 import {connect} from "react-redux";
 
 const Comical = (props) =>{
-    const upgrade =()=>{
-        let newUser = {...props.user};
-        newUser.score.comic +=1;
-        props.upgrade(newUser);
+    const upgrade =(result)=>{
+
+        console.log(result);
+        if(result === '<xml xmlns="https://developers.google.com/blockly/xml"><variables><variable id="s,o~Fq{.WT1~Gtn24*]}">Steps</variable></variables><block type="variables_set" id="-b+C,,dSEh3D,jP\`w9M/" x="247" y="68"><field name="VAR" id="s,o~Fq{.WT1~Gtn24*]}">Steps</field><value name="VALUE"><block type="math_number" id="[m:I.YlMsrtyL?b9q/RU"><field name="NUM">10</field></block></value></block></xml>'){
+            let newUser = {...props.user};
+            newUser.score.comic +=1;
+            props.upgrade(newUser);
+        }
+
     }
      return(
          <>
              {props.user.score.comic ===1?
-                 <FirstLevelPage
+                 <FirstLevelPage Upgrade={upgrade}
                  user={props.user}
                  logout={props.logout}
              />:props.user.score.comic ===2?
@@ -27,7 +32,6 @@ const Comical = (props) =>{
                  logout={props.logout}
                  />:<h1>You are ready!</h1>
              }
-             <button onClick={ upgrade }>Upgrade</button>
          </>
      )
 };
