2c0b6210c2cf9392d17061c2f6d86b1f25665257
Nawal-Dweik <nawal_farouk@outlook.com>
2021-02-24 17:50:23 +0200

    fix content & style for dashboard & home

--- fun_learning/src/levels/FirstLevelPage.js
+++ fun_learning/src/levels/FirstLevelPage.js
@@ -1,17 +1,10 @@
 import React from "react";
 import Footer from "../views/Footer";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import DashboardMain from "../static/img/dashboard_main.jpg";
-import Variable from "../static/img/variable_icon.png";
 import Header from "../views/Header";
 import ReactBlockly from "react-blockly";
 import Blockly from "blockly";
